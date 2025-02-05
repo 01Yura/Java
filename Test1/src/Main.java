01e0c718639f4c6b5c1d2134c6b212253b7ae742
Yura Primyshev <p020885@yandex.ru>
2025-02-05 01:08:29 -0500

    1

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,9 +1,9 @@
-class Car {  // Класс Car
+class Cars {  // Класс Car
     String model;
     int year;
     boolean isDriving;
 
-    public Car(String m, int y, boolean drive) {  // Конструктор
+    public Cars(String m, int y, boolean drive) {  // Конструктор
         model = m;
         year = y;
         isDriving = drive;
@@ -18,7 +18,7 @@ class Car {  // Класс Car
 
 public class Main {  // Главный класс с методом main
     public static void main(String[] args) {
-        Car myCar = new Car("Toyota", 1985, true);  // Создаём объект
+        Cars myCar = new Cars("Toyota", 1985, true);  // Создаём объект
         myCar.drive();  // Вызываем метод
         Moto myMoto = new Moto();
         System.out.println(myMoto.year);
