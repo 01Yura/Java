c04008d0022174d6158459dc4775f533999ac5ff
Yura Primyshev <p020885@yandex.ru>
2025-02-07 01:20:33 -0500

    1

---
+++ Test1/src/Test1.java
@@ -0,0 +1,12 @@
+import java.net.SocketOption;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String a = "Suslik";
+        int x = 333;
+        String s = a + x;
+        System.out.println(s);
+
+    }
+}
